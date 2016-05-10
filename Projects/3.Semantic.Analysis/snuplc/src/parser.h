89e867700b81fa10c77788db1f5235337e9fdb90
xdoju <xdoju.l@gmail.com>
2016-05-10 18:41:19 +0900

    fetch files from project 2

---
+++ Projects/3.Semantic.Analysis/snuplc/src/parser.h
@@ -0,0 +1,220 @@
+//------------------------------------------------------------------------------
+/// @brief SnuPL/0 parser
+/// @author Bernhard Egger <bernhard@csap.snu.ac.kr>
+/// @section changelog Change Log
+/// 2012/09/14 Bernhard Egger created
+/// 2013/03/07 Bernhard Egger adapted to SnuPL/0
+/// 2016/03/09 Bernhard Egger adapted to SnuPL/!
+/// 2016/04/08 Bernhard Egger assignment 2: parser for SnuPL/-1
+///
+/// @section license_section License
+/// Copyright (c) 2012-2016, Bernhard Egger
+/// All rights reserved.
+///
+/// Redistribution and use in source and binary forms,  with or without modifi-
+/// cation, are permitted provided that the following conditions are met:
+///
+/// - Redistributions of source code must retain the above copyright notice,
+///   this list of conditions and the following disclaimer.
+/// - Redistributions in binary form must reproduce the above copyright notice,
+///   this list of conditions and the following disclaimer in the documentation
+///   and/or other materials provided with the distribution.
+///
+/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+/// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO,  THE
+/// IMPLIED WARRANTIES OF MERCHANTABILITY  AND FITNESS FOR A PARTICULAR PURPOSE
+/// ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER  OR CONTRIBUTORS BE
+/// LIABLE FOR ANY DIRECT,  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSE-
+/// QUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  SUBSTITUTE
+/// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+/// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  CONTRACT, STRICT
+/// LIABILITY, OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY WAY
+/// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+/// DAMAGE.
+//------------------------------------------------------------------------------
+
+#ifndef __SnuPL_PARSER_H__
+#define __SnuPL_PARSER_H__
+
+#include "scanner.h"
+#include "symtab.h"
+#include "ast.h"
+
+
+//------------------------------------------------------------------------------
+/// @brief parser
+///
+/// parses a module
+///
+class CParser {
+  public:
+    /// @brief constructor
+    ///
+    /// @param scanner  CScanner from which the input stream is read
+    CParser(CScanner *scanner);
+
+    /// @brief parse a module
+    /// @retval CAstNode program node
+    CAstNode* Parse(void);
+
+    /// @name error handling
+    ///@{
+
+    /// @brief indicates whether there was an error while parsing the source
+    /// @retval true if the parser detected an error
+    /// @retval false otherwise
+    bool HasError(void) const { return _abort; };
+
+    /// @brief returns the token that caused the error
+    /// @retval CToken containing the error token
+    const CToken* GetErrorToken(void) const;
+
+    /// @brief returns a human-readable error message
+    /// @retval error message
+    string GetErrorMessage(void) const;
+    ///@}
+
+  private:
+    /// @brief sets the token causing a parse error along with a message
+    /// @param t token causing the error
+    /// @param message human-readable error message
+    void SetError(CToken t, const string message);
+
+    /// @brief consume a token given type and optionally store the token
+    /// @param type expected token type
+    /// @param token If not null, the consumed token is stored in 'token'
+    /// @retval true if a token has been consumed
+    /// @retval false otherwise
+    bool Consume(EToken type, CToken *token=NULL);
+
+    /// @brief initialize symbol table
+    /// @param s with predefined procedures and global variables
+    void InitSymbolTable(CSymtab *s);
+
+    /// @name methods for recursive-descent parsing
+    /// @{
+
+    /// @brief takes entire module and make AST node
+    /// @retval CAstModule* node
+    CAstModule*       module(void);
+
+    /// @brief takes a set of variable declaration and add them into the given symbol table
+    /// @param symbols a given symbol table
+    /// @param s_type a symbol type depend on the location of variable declaration
+    /// @retval CSymtab* symbols which saves newly declared symbols
+    CSymtab* varDeclaration(CSymtab* symbols, ESymbolType s_type);
+
+    /// @brief takes an one type of the variable declaration and add them into the given symbol table
+    /// @param symbols a given symbol table
+    /// @param s_type a symbol type depend on the location of variable declaration
+    /// @param *params a pointer to vector<CSymParam*> which contains formal parameters of the subroutine, NULL otherwise
+    /// @retval CSymtab* symbols which saves newly declared symbols
+    /// @retval vector<CSymParam*>* as a parameter
+    CSymtab* varDecl(CSymtab* symbols, ESymbolType s_type, vector<CSymParam*> *params);
+
+    /// @brief takes a declaration part of subroutine and add it into global symbol table
+    /// @param s parent scope
+    /// @retval CAstProcedure node with its name, procedure symbol and its parent scope
+    CAstProcedure*    subroutineDecl(CAstScope *s);
+
+    /// @brief takes a type definition
+    /// @retval CAstType* node with its type
+    const CAstType* ReadType(void);
+
+    /// @brief takes a sequence of statements to make a linked list of them
+    /// @param s parent scope
+    /// @retval head of linked list of statements if it is non-empty
+    /// @retval NULL otherwise
+    CAstStatement*    statSequence(CAstScope *s);
+
+    /// @brief takes each statement
+    /// @param s parent scope
+    /// @retval CAstStatement* node if exists
+    /// @retval NULL otherwise
+    CAstStatement*    statement(CAstScope *s);
+
+    /// @brief takes an assignment statement
+    /// @param s parent scope
+    /// @retval CAstStatAssign* node with lhs and rhs
+    CAstStatAssign*   assignment(CAstScope *s);
+
+    /// @brief takes a subroutine call statement
+    /// @param s parent scope
+    /// @retval CAstStatCall* node with an CAstFunctionCall* node fc
+    CAstStatCall* subroutineCall(CAstScope *s);
+
+    /// @brief takes an if statement
+    /// @param s parent scope
+    /// @retval CAstStatIf* node with ifcondition, ifstat and elsestat
+    CAstStatIf* ifStatement(CAstScope *s);
+
+    /// @brief takes a while loop statement
+    /// @param s parent scope
+    /// @retval CAstStatWhile* node with whilecondition, whilebody
+    CAstStatWhile* whileStatement(CAstScope *s);
+
+    /// @brief takes a return statement
+    /// @param s parent scope
+    /// @retval CAstStateReturn* node with a return statement
+    CAstStatReturn* returnStatement(CAstScope *s);
+    /// @brief takes a factor like constants, qualident, parenthesized expression, negated factor
+    /// @param s
+    /// @retval CAstExpression* node with a factor
+    CAstExpression*   factor(CAstScope *s);
+
+    /// @brief takes a qualident as ident { "[" [ number ] "]" }
+    /// @param s parent scope
+    /// @retval CAstDesignator* node with the qualident's symbol
+    CAstDesignator* qualident(CAstScope* s);
+
+    /// @brief takes a subroutine call expression
+    /// @param s parent scope
+    /// @retval CAstFunctionCall* node with a procedure symbol
+    CAstFunctionCall* expSubroutineCall(CAstScope *s);
+
+    /// @brief takes a term as "factor { termOp factor }"
+    /// @param s parent scope
+    /// @retval CAstExpression* node with a term
+    CAstExpression*   term(CAstScope *s);
+
+    /// @brief takes a simpleexpr as "[ + | - ] term { termOp term }"
+    /// @param s parent scope
+    /// @retval CAstExpression* node with a simpleexpr
+    CAstExpression*   simpleexpr(CAstScope *s);
+
+    /// @brief takes an expression as "simpleexpr [ relOp simpleexpr ]"
+    /// @param s parent scope
+    /// @retval CAstExpression* node with an expression
+    CAstExpression*   expression(CAstScope *s);
+
+    /// @brief takes a number constant and check w.o.n it exceeds a boundary
+    /// @retval CAstConstant* node with integer type and value
+    CAstConstant*     number(void);
+
+    /// @brief takes "true" or "false"
+    /// @retval CAstConstant* node with boolean type and 1 if true, 0 if false
+    CAstConstant*     boolean(void);
+
+    /// @brief takes a character constant and unescape it
+    /// @retval CAstConstant* node with character type and its value
+    CAstConstant*     character(void);
+
+    /// @brief takes a string constant
+    /// @param s parent scope
+    /// @retval CAstStringConstant* node with its value and parent scope
+    CAstStringConstant* stringConst(CAstScope *s);
+
+    /// @}
+
+    CScanner     *_scanner;       ///< CScanner instance
+    CAstModule   *_module;        ///< root node of the program
+    CToken        _token;         ///< current token
+
+    /// @name error handling
+    CToken        _error_token;   ///< error token
+    string        _message;       ///< error message
+    bool          _abort;         ///< error flag
+
+};
+
+#endif // __SnuPL_PARSER_H__
---
+++ Projects/3.Semantic.Analysis/snuplc/src/scanner.cpp
@@ -0,0 +1,681 @@
+//------------------------------------------------------------------------------
+/// @brief SnuPL/0 scanner
+/// @author Bernhard Egger <bernhard@csap.snu.ac.kr>
+/// @section changelog Change Log
+/// 2012/09/14 Bernhard Egger created
+/// 2013/03/07 Bernhard Egger adapted to SnuPL/0
+/// 2014/09/10 Bernhard Egger assignment 1: scans SnuPL/-1
+/// 2016/03/13 Bernhard Egger assignment 1: adapted to modified SnuPL/-1 syntax
+///
+/// @section license_section License
+/// Copyright (c) 2012-2016, Bernhard Egger
+/// All rights reserved.
+///
+/// Redistribution and use in source and binary forms,  with or without modifi-
+/// cation, are permitted provided that the following conditions are met:
+///
+/// - Redistributions of source code must retain the above copyright notice,
+///   this list of conditions and the following disclaimer.
+/// - Redistributions in binary form must reproduce the above copyright notice,
+///   this list of conditions and the following disclaimer in the documentation
+///   and/or other materials provided with the distribution.
+///
+/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+/// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO,  THE
+/// IMPLIED WARRANTIES OF MERCHANTABILITY  AND FITNESS FOR A PARTICULAR PURPOSE
+/// ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER  OR CONTRIBUTORS BE
+/// LIABLE FOR ANY DIRECT,  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSE-
+/// QUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF  SUBSTITUTE
+/// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+/// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN  CONTRACT, STRICT
+/// LIABILITY, OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN ANY WAY
+/// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+/// DAMAGE.
+//------------------------------------------------------------------------------
+
+#include <iostream>
+#include <sstream>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <cstdio>
+
+#include "scanner.h"
+using namespace std;
+
+//------------------------------------------------------------------------------
+// token names
+//
+#define TOKEN_STRLEN 16
+
+//------------------------------------------------------------------------------
+// SnuPL/1 token types
+//
+char ETokenName[][TOKEN_STRLEN] = {
+  "tNumber",                        ///< an integer
+  "tIdent",                         ///< an identifier
+  "tCharacter",                     ///< a character. ASCIIchar, "\n", "\t"
+  "tString",                        ///< a string such as "hello".
+  "tBool",                          ///< true value or false value
+
+  "tPlusMinus",                     ///< '+' or '-'
+  "tMulDiv",                        ///< '*' or '/'
+  "tRelOp",                         ///< "=", "#", "<", "<=", ">", or ">="
+  "tAndOr",                         ///< "&&"
+  "tNot",                           ///< '!' before boolean value
+  "tAssign",                        ///< ":="
+  "tSemicolon",                     ///< a semicolon
+  "tColon",                         ///< a colon
+  "tComma",                         ///< a comma
+  "tDot",                           ///< a dot which represents EOF
+  "tLParen",                        ///< a left bracket
+  "tRParen",                        ///< a right bracket
+  "tLBracket",                      ///< a left large bracket '['
+  "tRBracket",                      ///< a right large bracket ']'
+
+  "tModule",                        ///< "module"
+  "tBegin",                         ///< "begin"
+  "tEnd",                           ///< "end"
+  "tIf",                            ///< "if"
+  "tThen",                          ///< "then"
+  "tElse",                          ///< "else"
+  "tWhile",                         ///< "while"
+  "tDo",                            ///< "do"
+  "tReturn",                        ///< "return"
+  "tVar",                           ///< "var"
+  "tProcedure",                     ///< "procedure"
+  "tFunction",                      ///< "function"
+  "tBoolean",                       ///< "boolean"
+  "tChar",                          ///< "char"
+  "tInteger",                       ///< "integer"
+  "tComment",                       ///< multi single lines starting from "//"
+
+  "tEOF",                           ///< end of file
+  "tIOError",                       ///< I/O error
+  "tUndefined",                     ///< undefined
+};
+
+
+//------------------------------------------------------------------------------
+// format strings used for printing tokens
+//
+
+char ETokenStr[][TOKEN_STRLEN] = {
+  "tNumber (%s)",                   ///< an integer
+  "tIdent (%s)",                    ///< an identifier
+  "tCharacter (%s)",                ///< a character. ASCIIchar, "\n", "\t"
+  "tString (%s)",                   ///< a string such as "hello".
+  "tBool (%s)",                     ///< true value or false value
+
+  "tPlusMinus (%s)",                ///< '+' or '-'
+  "tMulDiv (%s)",                   ///< '*' or '/'
+  "tRelOp (%s)",                    ///< relational operator
+  "tAndOr (%s)",                    ///< "&&", "||"
+  "tNot",                           ///< '!' before boolean value
+  "tAssign",                        ///< assignment operator
+  "tSemicolon",                     ///< a semicolon
+  "tColon",                         ///< a colon
+  "tComma",                         ///< a comma
+  "tDot",                           ///< a dot which represents EOF
+  "tLParen",                        ///< a left bracket
+  "tRParen",                        ///< a right bracket
+  "tLBracket",                      ///< a left large bracket '['
+  "tRBracket",                      ///< a right large bracket ']'
+
+  "tModule",                        ///< "module"
+  "tBegin",                         ///< "begin"
+  "tEnd",                           ///< "end"
+  "tIf",                            ///< "if"
+  "tThen",                          ///< "then"
+  "tElse",                          ///< "else"
+  "tWhile",                         ///< "while"
+  "tDo",                            ///< "do"
+  "tReturn",                        ///< "return"
+  "tVar",                           ///< "var"
+  "tProcedure",                     ///< "procedure"
+  "tFunction",                      ///< "function"
+  "tBoolean",                       ///< "boolean"
+  "tChar",                          ///< "char"
+  "tInteger",                       ///< "integer"
+  "tComment (%s)",                  ///< multi single lines starting from "//"
+
+  "tEOF",                           ///< end of file
+  "tIOError",                       ///< I/O error
+  "tUndefined (%s)",                ///< undefined
+};
+
+
+//------------------------------------------------------------------------------
+// reserved keywords
+//
+pair<const char*, EToken> Keywords[] =
+{
+  {"module", tModule}, {"begin", tBegin}, {"end", tEnd}, {"if", tIf},
+  {"then", tThen}, {"else", tElse}, {"while", tWhile}, {"do", tDo},
+  {"return", tReturn}, {"var", tVar}, {"procedure", tProcedure},
+  {"function", tFunction}, {"begin", tBegin}, {"boolean", tBoolean},
+  {"char", tChar}, {"integer", tInteger}, {"true", tBool}, {"false", tBool}
+};
+
+
+
+//------------------------------------------------------------------------------
+// CToken
+//
+CToken::CToken()
+{
+  _type = tUndefined;
+  _value = "";
+  _line = _char = 0;
+}
+
+CToken::CToken(int line, int charpos, EToken type, const string value)
+{
+  _type = type;
+  _value = escape(value);
+  _line = line;
+  _char = charpos;
+}
+
+CToken::CToken(const CToken &token)
+{
+  _type = token.GetType();
+  _value = token.GetValue();
+  _line = token.GetLineNumber();
+  _char = token.GetCharPosition();
+}
+
+CToken::CToken(const CToken *token)
+{
+  _type = token->GetType();
+  _value = token->GetValue();
+  _line = token->GetLineNumber();
+  _char = token->GetCharPosition();
+}
+
+const string CToken::Name(EToken type)
+{
+  return string(ETokenName[type]);
+}
+
+const string CToken::GetName(void) const
+{
+  return string(ETokenName[GetType()]);
+}
+
+ostream& CToken::print(ostream &out) const
+{
+  int str_len = _value.length();
+  str_len = TOKEN_STRLEN + (str_len < 64 ? str_len : 64);
+  char *str = (char*)malloc(str_len);
+  snprintf(str, str_len, ETokenStr[GetType()], _value.c_str());
+  out << dec << _line << ":" << _char << ": " << str;
+  free(str);
+  return out;
+}
+
+string CToken::escape(const string text)
+{
+  string s;
+
+  for(char c : text){ // Use c++0x scheme to be able to read string over NULL.
+    switch(c){
+      case '\n': s += "\\n"; break;
+      case '\t': s += "\\t"; break;
+      case '\0': s += "\\0"; break;
+      case '\'': s += "\\'"; break;
+      case '\"': s += "\\\""; break;
+      case '\\': s += "\\\\"; break;
+      default: s += c;
+    }
+  }
+
+  return s;
+}
+
+string CToken::unescape(const string text)
+{
+  string s;
+  bool afterEscape = false;
+  for(char c : text){
+    if(afterEscape){
+      afterEscape = false;
+      switch(c){
+        case 'n': s += "\n"; break;
+        case 't': s += "\t"; break;
+        case '0': s += "\0"; break;
+        case '\'': s += "'"; break;
+        case '"': s += "\""; break;
+        case '\\': s += "\\"; break;
+        default : s += '?';
+      }
+    }
+    else if(c == '\\'){
+      afterEscape = true;
+    }
+    else{
+      s += c;
+    }
+  }
+
+  return s;
+}
+
+ostream& operator<<(ostream &out, const CToken &t)
+{
+  return t.print(out);
+}
+
+ostream& operator<<(ostream &out, const CToken *t)
+{
+  return t->print(out);
+}
+
+
+//------------------------------------------------------------------------------
+// CScanner
+//
+map<string, EToken> CScanner::keywords;
+
+CScanner::CScanner(istream *in)
+{
+  InitKeywords();
+  _in = in;
+  _delete_in = false;
+  _line = _char = 1;
+  _token = NULL;
+  _good = in->good();
+  NextToken();
+}
+
+CScanner::CScanner(string in)
+{
+  InitKeywords();
+  _in = new istringstream(in);
+  _delete_in = true;
+  _line = _char = 1;
+  _token = NULL;
+  _good = true;
+  NextToken();
+}
+
+CScanner::~CScanner()
+{
+  if (_token != NULL) delete _token;
+  if (_delete_in) delete _in;
+}
+
+void CScanner::InitKeywords(void)
+{
+  if (keywords.size() == 0) {
+    int size = sizeof(Keywords) / sizeof(Keywords[0]);
+    for (int i=0; i<size; i++) {
+      keywords[Keywords[i].first] = Keywords[i].second;
+    }
+  }
+}
+
+CToken CScanner::Get()
+{
+  CToken result(_token);
+
+  EToken type = _token->GetType();
+  _good = !(type == tIOError);
+
+  NextToken();
+  return result;
+}
+
+CToken CScanner::Peek() const
+{
+  return CToken(_token);
+}
+
+void CScanner::NextToken()
+{
+  if (_token != NULL) delete _token;
+
+  _token = Scan();
+}
+
+void CScanner::RecordStreamPosition()
+{
+  _saved_line = _line;
+  _saved_char = _char;
+}
+
+void CScanner::GetRecordedStreamPosition(int *lineno, int *charpos)
+{
+  *lineno = _saved_line;
+  *charpos = _saved_char;
+}
+
+CToken* CScanner::NewToken(EToken type, const string token)
+{
+  return new CToken(_saved_line, _saved_char, type, token);
+}
+
+CToken* CScanner::Scan()
+{
+  EToken token;
+  string tokval;
+  char c;
+  do{
+  while (_in->good() && IsWhite(_in->peek())) GetChar();
+
+  RecordStreamPosition();
+
+  if (_in->eof()) return NewToken(tEOF);
+  if (!_in->good()) return NewToken(tIOError);
+
+  c = GetChar();
+  tokval = c;
+  token = tUndefined;
+
+  switch (c) {
+
+    case '+':
+    case '-':
+      token = tPlusMinus;
+      break;
+
+    case '*':
+      token = tMulDiv;
+      break;
+
+    case '/':
+      token = tMulDiv;
+
+      if(_in->peek() == '/'){ // comment!
+        token = tComment;
+
+        for(;;){
+          char t = _in->peek();
+          if(t == '\n') break;
+          tokval += GetChar();
+        }
+      }
+      break;
+
+    case '&':
+    case '|':
+      if(c == _in->peek()){
+        token = tAndOr;
+        tokval += GetChar();
+      }
+      else{
+        token = tUndefined;
+      }
+      break;
+
+    case '!':
+      token = tNot;
+      break;
+
+    case '=':
+    case '#':
+      token = tRelOp;
+      break;
+
+    case '<':
+      if(_in->peek() == '=') {
+        tokval += GetChar();
+        token = tRelOp;
+      }
+      else {
+        token = tRelOp;
+      }
+      break;
+
+    case '>':
+      if(_in->peek() == '=') {
+        tokval += GetChar();
+        token = tRelOp;
+      }
+      else {
+        token = tRelOp;
+      }
+      break;
+
+    case ':':
+      if (_in->peek() == '=') {
+        tokval += GetChar();
+        token = tAssign;
+      }
+      else{
+        token = tColon;
+      }
+      break;
+
+    case ';':
+      token = tSemicolon;
+      break;
+
+    case '.':
+      token = tDot;
+      break;
+
+    case ',':
+      token = tComma;
+      break;
+
+    case '(':
+      token = tLParen;
+      break;
+
+    case ')':
+      token = tRParen;
+      break;
+
+    case '[':
+      token = tLBracket;
+      break;
+
+    case ']':
+      token = tRBracket;
+      break;
+
+    case '\'':
+      {
+        tokval = "";
+
+        bool valid = true;
+        int len = 0;
+
+        for(;;){
+          if(_in->good()){
+            char t = _in->peek();
+            if(t == EOF){ // Pick EOF not to put EOF in character
+              token = tUndefined;
+              break;
+            }
+            else t = GetChar();
+            if(t == '\\'){ // Escape
+              if(_in->good()){ // Check w.o.n next input stream is good
+                t = _in->peek();
+                switch(t){
+                  case 'n':
+                    GetChar(); t = '\n'; break;
+                  case 't':
+                    GetChar(); t = '\t'; break;
+                  case '\'':
+                    GetChar(); t = '\''; break;
+                  case '\"':
+                    GetChar(); t = '\"'; break;
+                  case '\\':
+                    GetChar(); t = '\\'; break;
+                  case '0':
+                    GetChar(); t = '\0'; break;
+                  case EOF: // Pick EOF not to put EOF in character
+                    valid = false; break;
+                  default: // Invalid
+                    GetChar(); valid = false;
+                    break;
+                }
+              }
+              else{
+                token = tUndefined; valid = false;
+                break;
+              }
+            }
+            else if(t == '\''){ // Close quote
+              if(len == 1 && valid){
+                // We assumed that we have to read all characters from opening
+                // quote to closing quote and make it one token.
+                token = tCharacter;
+              }
+              else token = tUndefined;
+              break;
+            }
+            else if(!IsAscii(t)){
+              valid = false;
+            }
+            tokval += t;
+            len++;
+          }
+          else{
+            token = tUndefined;
+            break;
+          }
+        }
+        break;
+      }
+
+    case '\"':
+      {
+        tokval = "";
+
+        bool valid = true;
+
+        for(;;){
+          if(_in->good()){
+            char t = _in->peek();
+            if(t == EOF){ // Pick EOF not to put EOF in string
+              token = tUndefined;
+              break;
+            }
+            else t = GetChar();
+            if(t == '\\'){ // Escape
+              if(_in->good()){ // Check w.o.n next input stream is good
+                t = _in->peek();
+                switch(t){
+                  case 'n':
+                    GetChar(); t = '\n'; break;
+                  case 't':
+                    GetChar(); t = '\t'; break;
+                  case '\'':
+                    GetChar(); t = '\''; break;
+                  case '\"':
+                    GetChar(); t = '\"'; break;
+                  case '\\':
+                    GetChar(); t = '\\'; break;
+                  case '0':
+                    GetChar(); t = '\0'; break;
+                  case EOF: // Pick EOF not to put EOF in string
+                    valid = false; break;
+                  default: // Invalid
+                    GetChar(); valid = false;
+                    break;
+                }
+              }
+              else{
+                token = tUndefined; valid = false;
+                break;
+              }
+            }
+            else if(t == '\"'){ // close quote
+              if(valid){
+                // We assumed that we have to read all characters from opening
+                // quote to closing quote and make it one token.
+                token = tString;
+              }
+              else token = tUndefined;
+              break;
+            }
+            else if(!IsAscii(t)){
+              valid = false;
+            }
+            tokval += t;
+          }
+          else{
+            token = tUndefined;
+            break;
+          }
+        }
+        break;
+      }
+
+    default:
+      if(IsDigit(c)){ // First detect digit.
+        token = tNumber;
+
+        char t = _in->peek();
+        while(IsDigit(t)){
+          tokval += GetChar();
+          t = _in->peek();
+        }
+        break;
+      }
+
+      else if(IsLetter(c)){ // Else detect letter.
+        token = tIdent;
+
+        char t = _in->peek();
+        while (IsLetter(t) || IsDigit(t)){
+          tokval += GetChar();
+          t = _in->peek();
+        }
+
+        if(keywords.find(tokval) != keywords.end()) { // Find existing keyword
+          token = keywords.find(tokval)->second;
+        }
+        else {
+          token = tIdent;
+          keywords[tokval] = token;
+        }
+        break;
+      }
+
+      else {
+        tokval = "invalid character ("; // To make consistency, changed ''' to '('
+        tokval += c;
+        tokval += ")";
+      }
+      break;
+  }
+  }while(token == tComment);
+
+  return NewToken(token, tokval);
+}
+
+char CScanner::GetChar()
+{
+  char c = _in->get();
+  if (c == '\n') { _line++; _char = 1; } else _char++;
+  return c;
+}
+
+string CScanner::GetChar(int n)
+{
+  string str;
+  for (int i=0; i<n; i++) str += GetChar();
+  return str;
+}
+
+bool CScanner::IsDigit(char c) const { // Digit checker
+  return (c >= '0' && c <= '9');
+}
+
+bool CScanner::IsLetter(char c) const { // Letter checker
+  return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c == '_');
+}
+
+bool CScanner::IsAscii(char c) const{ // Ascii checker
+  return ((0x20 <= c) && (c <= 0x7E));
+}
+
+bool CScanner::IsWhite(char c) const // Whitespace checker
+{
+  return ((c == ' ') || (c == '\n') || (c == '\t'));
+}
